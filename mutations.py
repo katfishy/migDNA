90866ef3b58f7ad3ecc111b0c05efcd4494dcb7d
katfishy <166878958+katfishy@users.noreply.github.com>
2024-08-26 15:40:32 -0700

    improve readability of function docstrings

--- mutations.py
+++ mutations.py
@@ -1,59 +1,59 @@
 import random
 
 Nucleotides = ['A', 'T', 'G', 'C']
-example = 'ABCDEFGHIJK'
 
 def randinsert(seq: str) -> str:
-    """A random insertion mutation in the DNA sequence."""
+    """Return the DNA seq with a random single nucleotide insertion mutation."""
     rand = random.randint(0, len(seq))
     new_seq = seq[:rand] + random.choice(Nucleotides) + seq[rand:]
     return new_seq
 
 def randdel(seq: str) -> str:
-    """A random deletion mutation in the DNA sequence."""
+    """Return the DNA seq with a random single nucleotide deletion mutation."""
     rand = random.randint(0, len(seq))
     new_seq = seq[:rand] + seq[rand + 1:]
     return new_seq
 
 def randsub(seq: str) -> str:
-    """A random substitution mutation in the DNA sequence."""
+    """Return the DNA seq with a random single nucleotide substitution mutation."""
     rand = random.randint(0, len(seq) - 1)
     new_seq = seq[:rand] + random.choice(Nucleotides) + seq[rand + 1:]
     return new_seq
 
-def insertion(seq: str, pos: int, nuc: str) -> str:
-    """A single insertion mutation of a specific nucleotide in the DNA 
-    sequence before a specific position."""
-    position = pos - 1
-    new_seq = seq[:position] + nuc + seq[position:]
+def insertion(seq: str, position: int, nucleotide: str) -> str:
+    """Return the DNA seq with a single insertion mutation of a specific nucleotide
+    at a specific position.
+    """
+    pos = position - 1
+    new_seq = seq[:pos] + nucleotide + seq[pos:]
     return new_seq
 
-def deletion(seq: str, pos: int) -> str:
-    """A single deletion mutation in the DNA sequence at a specific position."""
-    position = pos - 1
-    new_seq = seq[:position] + seq[position + 1:]
+def deletion(seq: str, position: int) -> str:
+    """Return the DNA seq with a single deletion mutation at a specific position."""
+    pos = position - 1
+    new_seq = seq[:pos] + seq[pos + 1:]
     return new_seq
 
-def substitution(seq: str, pos: int, nuc: str) -> str:
-    """A single substitution mutation of a specific nucleotide in the DNA sequence
-    before a specific position."""
-    position = pos - 1
-    new_seq = seq[:position] + nuc + seq[position + 1:]
+def substitution(seq: str, position: int, nucleotide: str) -> str:
+    """Return the DNA seq with a single substitution mutation of a specific nucleotide 
+    in the DNA sequence before a specific position.
+    """
+    pos = position - 1
+    new_seq = seq[:pos] + nucleotide + seq[pos + 1:]
     return new_seq
 
 def seqtoinsert(seq1: str, seq2: str) -> bool:
-    """Returns True if seq2 is an insertion mutation of seq1"""
+    """Return True if seq2 is an insertion mutation of seq1."""
     return len(seq2) == len(seq1) + 1
 
 def seqtodel(seq1: str, seq2: str) -> bool:
-    """Returns True if seq2 is an deletion mutation of seq1"""
+    """Return True if seq2 is a deletion mutation of seq1."""
     return len(seq2) == len(seq1) - 1
 
 def seqtosub(seq1: str, seq2: str) -> bool:
-    """Returns True if seq2 is an substitution mutation of seq1"""
+    """Return True if seq2 is a substitution mutation of seq1."""
     for i in range(len(seq1)):
         if seq1[i] != seq2[i]:
             return True
-        else:
-            return False
+    return False
 
